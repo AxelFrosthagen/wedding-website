2c181cdaae5c2a9a2d26ba428a29ac37d2a4bcf0
Axel Frosthagen <axel.frosthagen@gmail.com>
2025-05-24 10:28:04 +0200

    Added base for form submission

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    /********************** RSVP **********************/
+    $('#rsvp-form').on('submit', function(e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+
+        $.post('https://script.google.com/macros/s/AKfycbygFXdiDQ60lX0v2pKUJAfBeb5qlV79zbjZrODzVckwGzd8FNuRm10H3H_N7uUpRZM/exec', data)
+            .done(function(data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
+                }
+            })
+            .fail(function(data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
+    });
+
+    // alert_markup
+    function alert_markup(alert_type, msg) {
+        return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
+    }
+
+});
